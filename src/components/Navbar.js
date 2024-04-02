2692643d3c696f748fcc5611cca4b1e4a1682866
Artisable <artisabale20@gmail.com>
2024-04-02 17:17:12 +0530

    Add changes

---
+++ src/components/Navbar.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+class Navbar extends React.Component {
+    state = {  } 
+    render() { 
+        return (
+            <h1>This is navbar</h1>
+        );
+    }
+}
+ 
+export default Navbar;
